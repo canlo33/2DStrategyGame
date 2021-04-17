0372c7e1add56d73181fb249996282bdca9072a9
Can LO <43052031+canlo33@users.noreply.github.com>
2021-04-17 16:32:14 -0400

    Camera Controller Implementatiton
    1) Now we have a virtual camera that player can control.
    
    2) Camera can be moved horizontally and vertically.
    
    3)Camera  zoom in and out functionality is also added so the player can zoom in and out using mouse wheel.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private CinemachineVirtualCamera cinemachineCamera;
+    private float zoomAmount;
+    private float targetZoomAmount;
+    public float movementSpeed = 30f;
+    public float zoomSpeed = 2f;
+    private void Start()
+    {
+        zoomAmount = cinemachineCamera.m_Lens.OrthographicSize;
+        targetZoomAmount = zoomAmount;
+    }
+    void Update()
+    {
+        Move();
+        Zoom();
+    }
+    //This function allows us the move the camera horizontally and vertically using the arrow keys.
+    void Move()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+        Vector3 moveDirection = new Vector3(x, y).normalized;
+        transform.position += moveDirection * movementSpeed * Time.deltaTime;
+    }
+    //This function allows us to zoom the camera in and out using the mousewheel.
+    // I made it so when we zoom in and out zooming is happening smoothly rather than instant.
+    //It also looks the zoom values so we cant zoom in or out too much.
+    void Zoom()
+    {
+        //Zoom calculation.
+        targetZoomAmount -= Input.mouseScrollDelta.y * zoomSpeed;
+
+        //Clamping the zoom values.
+        float minZoom = 5f;
+        float maxZoom = 20f;
+        targetZoomAmount = Mathf.Clamp(targetZoomAmount, minZoom, maxZoom);
+
+        //Smoothing the zoom.
+        zoomAmount = Mathf.Lerp(zoomAmount, targetZoomAmount, Time.deltaTime * 5f);
+        cinemachineCamera.m_Lens.OrthographicSize = zoomAmount;
+    }
+}
