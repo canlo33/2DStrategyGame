7915049734ad625be0c9df79cf4036fe54953f8d
Can LO <43052031+canlo33@users.noreply.github.com>
2021-05-14 02:11:20 -0400

    Enemy Wave Manager Implementation
    1) Now there is an enemy wave manager which is responsible of respawning  enemies and creating waves that grow stronger after each wave.
    
    2)Visual Updates: Shaders are added to make the enemies and the arrows glow and looks better.

---
+++ Assets/Scripts/UI/EnemyWaveUI.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class EnemyWaveUI : MonoBehaviour
+{
+    [SerializeField] private WaveManager waveManager;
+    [SerializeField] private TextMeshProUGUI waveNumberText;
+    [SerializeField] private TextMeshProUGUI waveMessageText;
+    [SerializeField] private RectTransform wavePositionIndicator;
+    private Camera mainCamera;
+
+    private void Start()
+    {
+        mainCamera = Camera.main;
+        waveManager.OnWaveNumberChanged += WaveManager_OnWaveNumberChanged;
+        SetNumberText("Wave " + waveManager.GetCurrentWaveNumber());
+    }
+
+    private void WaveManager_OnWaveNumberChanged(object sender, System.EventArgs e)
+    {
+        SetNumberText("Wave " + waveManager.GetCurrentWaveNumber());
+    }
+    private void Update()
+    {
+        UpdateWaveTimer();
+        UpdateWavePositionIndicator();
+    }
+    private void UpdateWavePositionIndicator()
+    {
+        //Point the WavePositionIndicator towards to the wave location and offset it towards that direction
+        Vector3 nextSpawnDirection = (waveManager.GetSpawnPosition() - mainCamera.transform.position).normalized;
+        wavePositionIndicator.anchoredPosition = nextSpawnDirection * 300f;
+        wavePositionIndicator.eulerAngles = new Vector3(0f, 0f, UtilsClass.GetAngleFromVector(nextSpawnDirection));
+    }
+    private void UpdateWaveTimer()
+    {
+        float nextWaveTimer = waveManager.GetNextWaveTimer();
+        if (nextWaveTimer <= 0f)
+            SetMessageText("");
+        else
+            SetMessageText("Next Wave in " + nextWaveTimer.ToString("F0") + "s");
+
+    }
+    private void SetMessageText(string message)
+    {
+        waveMessageText.text = message;
+    }
+    private void SetNumberText(string message)
+    {
+        waveNumberText.text = message;
+    }
+}
---
+++ Assets/Scripts/WaveManager.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveManager : MonoBehaviour
+{
+    private enum State
+    {
+        WaitingForNextWave,
+        SpawningWave,
+    }
+    public event EventHandler OnWaveNumberChanged;
+    private State currentState;
+    private int currentWaveNumber;
+    private float nextWaveTimer;
+    private float nextWaveCooldown;
+    private float nextEnemySpawnTimer;
+    private int remainingEnemyToSpawn;
+    [SerializeField] private List<Transform> spawnPointList;
+    [SerializeField] private Transform wavePortal;
+    private Vector3 spawnPosition;
+    private void Start()
+    {
+
+        currentState = State.WaitingForNextWave;
+        spawnPosition = spawnPointList[UnityEngine.Random.Range(0, spawnPointList.Count)].position;
+        wavePortal.position = spawnPosition;
+        nextWaveTimer = 3f;
+    }
+    private void Update()
+    {
+        //Check which state we are currently at. If we are waiting for the next wave then just run the timer for nextWaveTimer.
+        //If we are spawning enemies, then run nextEnemySpawnTimer and start spawning enemies one after another.
+        switch (currentState)
+        {
+            case State.WaitingForNextWave:
+                nextWaveTimer -= Time.deltaTime;
+                if (nextWaveTimer < 0)
+                    SpawnWave();
+                break;
+            case State.SpawningWave:
+                if (remainingEnemyToSpawn > 0)
+                {
+                    nextEnemySpawnTimer -= Time.deltaTime;
+                    if (nextEnemySpawnTimer < 0)
+                    {
+                        nextEnemySpawnTimer = UnityEngine.Random.Range(0f, 0.2f);
+                        EnemyController.RespownEnemy(spawnPosition + UtilsClass.GetRandomDirection() * UnityEngine.Random.Range(0f, 10f));
+                        remainingEnemyToSpawn--;
+                    }
+                    if (remainingEnemyToSpawn <= 0)
+                    {
+                        currentState = State.WaitingForNextWave;
+                        spawnPosition = spawnPointList[UnityEngine.Random.Range(0, spawnPointList.Count)].position;
+                        wavePortal.position = spawnPosition;
+                        nextWaveTimer = 10f;
+                    }                        
+                }
+                break;
+        }
+    }
+    private void SpawnWave()
+    {        
+        remainingEnemyToSpawn = 5 + currentWaveNumber * 3;
+        currentState = State.SpawningWave;
+        currentWaveNumber++;
+        OnWaveNumberChanged?.Invoke(this, EventArgs.Empty);
+    }
+    public int GetCurrentWaveNumber()
+    {
+        return currentWaveNumber;
+    }
+    public float GetNextWaveTimer()
+    {
+        return nextWaveTimer;
+    }
+    public Vector3 GetSpawnPosition()
+    {
+        return spawnPosition;
+    }
+}
